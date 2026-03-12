3e0dbbc38cfc337dc22e227ada4e0202a86a3c70
cottonluxy-space <cottonluxy@gmail.com>
2026-03-12 22:59:50 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+// Supabase connection
+
+const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co"
+const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY"
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+)
+
+export default supabase
