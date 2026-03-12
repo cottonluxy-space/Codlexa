fe1a876066b81560a40c65ad14fd63f57e58e271
cottonluxy-space <cottonluxy@gmail.com>
2026-03-12 23:03:19 +0530

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,17 @@
+// Simple navigation helper
+
+function goHome() {
+  window.location.href = "index.html"
+}
+
+function goStudio() {
+  window.location.href = "playground.html"
+}
+
+function goAPI() {
+  window.location.href = "api.html"
+}
+
+function goPlans() {
+  window.location.href = "plans.html"
+}
